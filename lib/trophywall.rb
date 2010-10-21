18c39f2d9a5331b9ce8fdf8d4d3b42a25baaf160
Adrian Cuadros <adrian@innku.com>
2010-10-21 16:37:31 -0500

    Can now choose to log everything to tropywall

--- lib/trophywall.rb
+++ lib/trophywall.rb
@@ -3,11 +3,21 @@ module TrophyWall
   mattr_accessor :app_token
   @@app_token = nil
   
+  mattr_accessor :user_class
+  @@user_class = 'User'
+  
+  mattr_accessor :log_everything
+  @@user_class = false
+  
   @@app = nil
   
   def self.setup
     yield self
+    
     self.login 
+    
+    ActiveRecord::Base.send :include, TrophyWall::Challenge if @@log_everything
+    
   end
   
   def self.login
@@ -16,6 +26,10 @@ module TrophyWall
   
   def self.app
     @@app
+  end
+  
+  def self.call_user
+    self.user_class.downcase
   end
   
   if defined?(Rails)
--- lib/trophywall/challenge.rb
+++ lib/trophywall/challenge.rb
@@ -3,21 +3,75 @@ module TrophyWall
     
     def self.included(base)
       base.send :extend, ClassMethods
-      base.send :hit_on_create
+      base.send :include, InstanceMethods
+      base.send :trophywall_hit_on_create
     end
     
     module ClassMethods
       
-      def hit_on_create
-        after_create do |record|
-          TrophyWall.app.hit(action_name(self.class), self.user)
-        end
+      def challenge_name(name)
+        self.cattr_accessor :trophywall_challenge_name
+        self.trophywall_challenge_name = name.to_s
+      end
+      
+      def challenger(challenger_call)
+        self.cattr_accessor :trophywall_challenger
+        self.trophywall_challenger = challenger_call.to_s
+      end
+      
+      def not_challenging_for_trophywall
+        self.cattr_accessor :challenging_for_trophywall
+        self.challenging_for_trophywall = false
       end
             
+      private
+      
+      def trophywall_hit_on_create
+        after_create do |record|
+          if challenging_for_trophywall?
+            trophywall_hit(create_challenge_name)
+          end
+        end
+      end
+      
     end
     
-    def action_name(class_reference)
-      "create-#{class_reference.name.downcase}"
+    module InstanceMethods
+      
+      def trophywall_hit(action, user=nil)
+        TrophyWall.app.hit(action, challenger)
+      end
+      
+      private
+      
+      def challenger
+        if self.class.respond_to? :trophywall_challenger
+          self.send self.class.trophywall_challenger
+        else
+          self.send :user
+        end
+      end
+      
+      def create_challenge_name
+        if self.class.respond_to? :trophywall_challenge_name
+          self.class.trophywall_challenge_name
+        else
+          trophywall_action_name('create')
+        end
+      end
+
+      def trophywall_action_name(action)
+        "#{action}-#{self.class.name.downcase}"
+      end
+      
+      def challenging_for_trophywall?
+        if self.class.respond_to? :challenging_for_trophywall
+          self.class.challenging_for_trophywall
+        else
+          true
+        end
+      end
+      
     end
     
   end
